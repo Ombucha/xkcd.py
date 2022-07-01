f85c5b11df10e659edacfc06a2accb7fdd210379
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-07-01 17:02:15 +0530

    Bump to `v1.0.2`

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.rst").read_text()
 
 setup(
     name = "xkcd.py",
-    version = "1.0.1",
+    version = "1.0.2",
     description = "A basic API wrapper for xkcd.",
     long_description = README,
     long_description_content_type = "text/x-rst",
